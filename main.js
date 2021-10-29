ccecc80bca702b52b0c5ad7f5a6e8a755db84bec
hitomizatou <93360153+hitomizatou@users.noreply.github.com>
2021-10-29 12:04:11 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ $(function () {
     infinite: true,//スライドをループさせるかどうか。初期値はtrue。
     slidesToShow: 1,//スライドを画面に3枚見せる
     slidesToScroll: 1,//1回のスクロールで3枚の写真を移動して見せる
-    arrows: true,//左右の矢印あり
+    arrows: false,//左右の矢印なし
     prevArrow: '<div class="slick-prev"></div>',//矢印部分PreviewのHTMLを変更
     nextArrow: '<div class="slick-next"></div>',//矢印部分NextのHTMLを変更
     dots: true,//下部ドットナビゲーションの表示
